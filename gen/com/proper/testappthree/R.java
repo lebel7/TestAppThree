01efdb1686da69534398794fbe345f18deab7b93
lebel7 <lebel@mail.com>
2014-01-28 10:48:57 +0000

    main layout background changed

--- gen/com/proper/testappthree/R.java
+++ gen/com/proper/testappthree/R.java
@@ -24,11 +24,13 @@ public final class R {
     public static final class drawable {
         public static final int barcode_ean13=0x7f020000;
         public static final int details_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int misty_green_bag=0x7f020003;
-        public static final int nopic=0x7f020004;
-        public static final int unnamed=0x7f020005;
-        public static final int view_details=0x7f020006;
+        public static final int drips=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int misty_green_bag=0x7f020004;
+        public static final int nopic=0x7f020005;
+        public static final int simplenavy2=0x7f020006;
+        public static final int unnamed=0x7f020007;
+        public static final int view_details=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080025;
